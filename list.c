423debf8e324c1735160260ab86891fc83bbce4b
lifei098909 <33709087+lifei098909@users.noreply.github.com>
2020-12-20 21:17:24 +0800

    Update list.c
    Update list.c
    add leetcode 92, 25 234

--- list.c
+++ list.c
@@ -38,6 +38,104 @@ struct ListNode* reverseList_a(struct ListNode* head)
 }
 /* leetcode 206 end */
 
+/* leetcode 92 start */
+/*
+ * leetcode 92.反转链表II
+ * 反转从位置 m 到 n 的链表。请使用一趟扫描完成反转。
+
+ * 说明: 1 ≤ m ≤ n ≤ 链表长度。
+ * 示例:
+ * 输入: 1->2->3->4->5->NULL, m = 2, n = 4
+ * 输出: 1->4->3->2->5->NULL
+ */
+struct ListNode *rear = NULL;
+struct ListNode *reverseN(struct ListNode* head, int n)
+{
+    if (n == 1) {
+        rear = head->next;
+        return head;
+    }
+    struct ListNode *last = reverseN(head->next, n - 1);
+    head->next->next = head;
+    head->next = rear;
+    return last;
+}
+
+struct ListNode* reverseBetween(struct ListNode* head, int m, int n){
+    if (m == 1)
+        return reverseN(head, n);
+    head->next = reverseBetween(head->next, m - 1, n - 1);
+    return head;
+}
+/* leetcode 92 end */
+
+
+/* leetcode 25 start */
+/*
+ * leetcode 25. K 个一组翻转链表
+ * 给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+ * k 是一个正整数，它的值小于或等于链表的长度。
+ * 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+ * 示例：
+ * 给你这个链表：1->2->3->4->5
+ * 当 k = 2 时，应当返回: 2->1->4->3->5
+ * 当 k = 3 时，应当返回: 3->2->1->4->5
+ */
+struct ListNode* reverseList_ab(struct ListNode* a, struct ListNode *b)
+{
+    struct ListNode* pre = NULL;
+    struct ListNode* cur = a;
+    struct ListNode* next = a;
+    
+    while (cur != b) {
+        next = cur->next;
+        cur->next = pre;
+        pre = cur;
+        cur = next;
+    }
+    return pre;
+}
+
+struct ListNode* reverseKGroup(struct ListNode* head, int k){
+    if (head == NULL)
+        return head;
+    struct ListNode* a = head;
+    struct ListNode* b = head;
+
+    for (int i = 0; i < k; i++) {
+        if (b == NULL)
+            return head;
+        b = b->next;
+    }
+    struct ListNode* newHead = reverseList_ab(a, b);
+    a->next = reverseKGroup(b, k);
+    
+    return newHead;
+}
+/* leetcode 25 end */
+
+/* leetcode 234 start */
+/*
+ * leetcode 234.回文链表
+ * 请判断一个链表是否为回文链表。
+ * 示例 1:
+ * 输入: 1->2->2->1
+ * 输出: true
+ */
+bool isPalindrome(struct ListNode* head){
+    struct ListNode *left = head;
+    struct ListNode *right = reverse(head);
+
+    while (left ) {
+        // printf("right->value = %d\n", right->val);
+        if (left->val != right->val)
+            return false;
+        left = left->next;
+        right = right->next;
+    }
+    return true;
+}
+/* leetcode 234 end */
 void test_reverseList(void)
 {
     struct ListNode a;
@@ -62,7 +160,10 @@ void test_reverseList(void)
         printf("%d\n", head->val);
         head = head->next;
     }
-    head = reverseList_a(&a);
+    //head = reverseList_a(&a);
+    //head = reverseN(&a, 3);
+    //head = reverseBetween(&a, 2, 4);
+    head = reverseKGroup(&a, 2);
     printf("After reverse : \n");
     while (head) {
         printf("%d\n", head->val);
