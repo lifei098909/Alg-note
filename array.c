7025e08c52def63d42da1778c74b94f6c4dd3725
lifei098909 <33709087+lifei098909@users.noreply.github.com>
2020-12-26 23:00:04 +0800

    Update array.c
    Update array.c

--- array.c
+++ array.c
@@ -47,3 +47,21 @@ void test_twoSum(void)
 
 }
 /* leetcode 1 end */
+
+/* leetcode 283 start
+ * 移动零
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ */
+void moveZeroes(int* nums, int numsSize)
+{
+    int cnt = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] != 0)
+            nums[cnt++] = nums[i];
+    }
+
+    while (cnt < numsSize)
+        nums[cnt++] = 0;
+}
+/* leetcode 283 end */
+
