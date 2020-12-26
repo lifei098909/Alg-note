d6445202b0879c17a3fed96e34ee7c990f7db48f
lifei098909 <33709087+lifei098909@users.noreply.github.com>
2020-12-26 11:39:16 +0800

    Update common.h
    Update common.h

--- common.h
+++ common.h
@@ -1,8 +1,6 @@
 #ifndef __COMMON_H
 #define __COMMON_H
 
-#define max(a, b) (a > b ? a : b)
-#define min(a, b) (a < b ? a : b)
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 
 int comp(const void *a, const void *b);
