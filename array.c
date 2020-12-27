9114634f0736ede4cb0d3a2562c3d83163b42caa
lifei098909 <33709087+lifei098909@users.noreply.github.com>
2020-12-27 22:14:56 +0800

    Update array.c
    Update array.c

--- array.c
+++ array.c
@@ -65,3 +65,20 @@ void moveZeroes(int* nums, int numsSize)
 }
 /* leetcode 283 end */
 
+/* leetcode 27 start
+ * 移除元素
+ * 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+ */
+int removeElement(int* nums, int numsSize, int val)
+{
+    int cnt = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] != val)
+            nums[cnt++] = nums[i];
+    }
+
+    return cnt;
+}
+/* leetcode 27 end */
+
