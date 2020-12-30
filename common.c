8ec26386f466859b1c00e45310cb93c0f959b2ad
lifei098909 <33709087+lifei098909@users.noreply.github.com>
2020-12-30 22:53:57 +0800

    Update common.c
    Update common.c

--- common.c
+++ common.c
@@ -1,7 +1,13 @@
 #include "common.h"
 
+/* 从小到大排列 */
 int comp(const void *a, const void *b)
 {
     return *(int*)a - *(int*)b;
 }
 
+/* 从大到小排列 */
+int comb(const void *a, const void *b)
+{
+    return *(int*)b - *(int*)a;
+}
