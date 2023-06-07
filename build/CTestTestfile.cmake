bfe5760f11f962b8ad44ee7045c7cceb7e50adba
lifei098909 <33709087+lifei098909@users.noreply.github.com>
2023-06-07 22:52:52 +0800

    delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/0_github/self/Algorithms_and_data_structures/vscode_project
-# Build directory: E:/0_github/self/Algorithms_and_data_structures/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
