7f497eb15ebf1e991255adb294ae59a803db0cad
lifei098909 <33709087+lifei098909@users.noreply.github.com>
2023-06-20 23:02:05 +0800

    create dp_test.c

---
+++ code_project/c/src/dp/dp_test.c
@@ -0,0 +1,17 @@
+/**
+ * Copyright (C), 2021-2023, github.com/lifei098909.
+ * File: dynamic_programming.c
+ * Created Time: 2023-06-20
+ * Author: lifei (lifei1100508@163.com)
+ */
+
+void test_dp(void)
+{
+
+}
+
+int main(void)
+{
+    test_dp();
+    return 0;
+}
